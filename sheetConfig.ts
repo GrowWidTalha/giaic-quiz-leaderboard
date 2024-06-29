2d6f2ba1d1616be4bc5e3aa4fe5c7842d768557d
GrowWithTalha <growwithtalha2@gmail.com>
2024-06-29 13:45:27 +0500

    Update sheetConfig.ts

--- sheetConfig.ts
+++ sheetConfig.ts
@@ -1,21 +1,28 @@
 export const sheetConfigs = [
     {
-        // huzair
-        quizName: "Class 01",
-        email: "Email Address",
+        key: "1fmN5xMAUjIhysA63xNCDgMZN9eOoVo0Pyg2D2Vvh2nE",
         name: "Name",
         score: "Score",
-        key: "1wQG1rd9BwV1wLAC6ByJh6vldh--5K4v40Ds_8sC5Wt8",
+        email: "Email address",
         totalMarks: 20,
+        quizName: "Class 08",
     },
     {
-        // noor
-        quizName: "Class 02",
-        email: "Email Address",
-        name: "Name",
+        // fasih
+        key: "1KwNBESXlwCPahMQTQO_7Ul3zBIUcusuwYu6RpD3Mc_A",
+        name: "Name:",
         score: "Score",
-        key: "1jP5GExFnptCrQ8liHzMFgqzcRzU4jmLdHNfsY9gtRRA",
+        email: "Email Address",
         totalMarks: 20,
+        quizName: "Class 05"
+    },
+    {
+        key: "1RR6YRDSNNQbWRaLB5zULMfLqE_SAnIqH5iMez7xTWXM",
+        quizName: "Class 04",
+        name: "Name",
+        email: "Email Address",
+        totalMarks: 20,
+        score: "Score",
     },
     // jaweria
     // { //alishba
@@ -28,32 +35,22 @@ export const sheetConfigs = [
     // },
     // khalid Ghan
     {
-        key: "1RR6YRDSNNQbWRaLB5zULMfLqE_SAnIqH5iMez7xTWXM",
-        quizName: "Class 04",
-        name: "Name",
+        // noor
+        quizName: "Class 02",
         email: "Email Address",
-        totalMarks: 20,
+        name: "Name",
         score: "Score",
+        key: "1jP5GExFnptCrQ8liHzMFgqzcRzU4jmLdHNfsY9gtRRA",
+        totalMarks: 20,
     },
     {
-        // fasih
-        key: "1KwNBESXlwCPahMQTQO_7Ul3zBIUcusuwYu6RpD3Mc_A",
-        name: "Name:",
-        score: "Score",
+        // huzair
+        quizName: "Class 01",
         email: "Email Address",
+        name: "Name",
+        score: "Score",
+        key: "1wQG1rd9BwV1wLAC6ByJh6vldh--5K4v40Ds_8sC5Wt8",
         totalMarks: 20,
-        quizName: "Class 05"
-    },
-        {
-            key: "1fmN5xMAUjIhysA63xNCDgMZN9eOoVo0Pyg2D2Vvh2nE",
-            name: "Name",
-            score: "Score",
-            email: "Email address",
-            totalMarks: 20,
-            quizName: "Class 08",
-        },
-    // {
-
-    // }
+    }
     // Add more sheet configurations as needed
 ];
